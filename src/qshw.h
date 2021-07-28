626bad6ac3a721f07d97580f726f8219900cc05b
Qiaodong Xiang <88073865+ZYMelaii@users.noreply.github.com>
2021-07-28 22:47:24 +0800

    Update qshw.h

--- src/qshw.h
+++ src/qshw.h
@@ -3,13 +3,23 @@
 
 #include "core.h"
 
-#define QSHW_WHITE	0
-#define QSHW_RED	1
-#define QSHW_GREEN	2
-#define QSHW_BLUE	3
-#define QSHW_YELLOW 4
-#define QSHW_PINK	5
-#define QSHW_CYAN	6
+#ifndef QSHELL_QSHW_H
+#define QSHELL_QSHW_H
+
+#include "core.h"
+
+#define QSHW_WHITE		0
+#define QSHW_RED		1
+#define QSHW_GREEN		2
+#define QSHW_BLUE		3
+#define QSHW_YELLOW 	4
+#define QSHW_MAGENTA	5
+#define QSHW_CYAN		6
+
+void qshw_print(int color, const char *format, ...);
+void qshw_write_prompt(shell_t *psh);
+
+#endif
 
 void qshw_print(int color, const char *format, ...);
 void qshw_write_prompt(shell_t *psh);
